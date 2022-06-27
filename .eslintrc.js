54773918a06adb760cf3bd0b9e9b41c646b2f317
Jose Victor <jose.victor.dnts@gmail.com>
2022-06-27 16:30:44 -0300

    chore: setup eslint, prettier and ts configs

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@next/next/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'simple-import-sort',
+    'sort-keys-custom-order-fix',
+    'unused-imports'
+  ],
+  rules: {
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    eqeqeq: ['error', 'always'],
+    'no-useless-escape': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': 'error',
+    'sort-keys-custom-order-fix/sort-keys-custom-order-fix': [
+      'error',
+      'custom',
+      {
+        caseSensitive: true,
+        orderBy: 'asc'
+      }
+    ],
+    'unused-imports/no-unused-imports-ts': 'error'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
