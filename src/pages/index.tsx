a08cc7b2e0e48710480fa4660bd92d20b1e04ce8
Jose Victor <jose.victor.dnts@gmail.com>
2022-06-27 16:38:31 -0300

    refact: override create next app files

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <main>lets beat it</main>
+
+      <footer>
+        <a href="" target="_blank" rel="noopener noreferrer">
+          Powered by{' '}
+          <span>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
