076d253125fc1b52e69d64626fc39b89be25b223
Jose Victor <jose.victor.dnts@gmail.com>
2022-07-04 13:53:45 -0300

    feat: create const paths and links

---
+++ src/common/const/links.ts
@@ -0,0 +1,17 @@
+export const links = {
+  email: 'jose@zevictor.me',
+
+  musicInspiration: 'https://www.youtube.com/watch?v=Js02m-7qHyE',
+
+  projects: {
+    carbona: 'https://www.carbona.io/',
+    websiteRepo: 'https://github.com/zevictordantas/zevictor-website',
+    wizardUi: 'https://wizard-ui-docs.vercel.app/'
+  },
+
+  social: {
+    github: 'https://github.com/zevictordantas',
+    linkedin: 'https://www.linkedin.com/in/jvictordantas/',
+    twitter: 'https://twitter.com/ze_vdantas'
+  }
+}
---
+++ src/common/const/paths.ts
@@ -0,0 +1,11 @@
+/**
+ * Implementation of paths based on this article:
+ * https://blog.logrocket.com/dealing-with-links-in-next-js/
+ */
+export const paths = {
+  homepage: '/',
+
+  posts: '/posts',
+
+  works: '/works'
+}
